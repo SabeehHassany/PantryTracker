49d2281bc06cf974733e3335bc7e50486ae73ca5
Sabeeh <61093501+SabeehHassany@users.noreply.github.com>
2024-08-07 16:44:39 -0400

    Fix to firebase.js file
    getAnalytics function is trying to access the window object, which is not available in a server-side environment. To fix this, you need to conditionally initialize Firebase Analytics only when running in a browser environment.

--- firebase.js
+++ firebase.js
@@ -1,12 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDK6ECtFLXz7dI6gOVmDlziHkSfnJyoE3M",
   authDomain: "inventory-manager-6daaa.firebaseapp.com",
@@ -19,8 +15,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app);
 
-export {firestore}
+let analytics;
+if (typeof window !== 'undefined') {
+  // Conditionally import and initialize analytics
+  import("firebase/analytics").then(({ getAnalytics }) => {
+    analytics = getAnalytics(app);
+  });
+}
 
+export { firestore };
